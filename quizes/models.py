8bbb40a3e58616ac3701646ab3b3643c6bd44428
Ali Mohammadi <ali_mohammadi@ut.ac.ir>
2022-11-27 19:03:38 +0330

    Add urls file to quizes app

--- quizes/models.py
+++ quizes/models.py
@@ -3,4 +3,3 @@ from django.db import models
 class Quiz(models.Model):
     question = models.TextField()
     user = models.ManyToManyField(question, related_name="question")
-
--- random_quiz/settings.py
+++ random_quiz/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'random_quiz1.urls'
+ROOT_URLCONF = 'random_quiz.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'random_quiz1.wsgi.application'
+WSGI_APPLICATION = 'random_quiz.wsgi.application'
 
 
 # Database
