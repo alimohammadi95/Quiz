a615314be0ef3020c62ac4733545dc7fc98182db
Ali Mohammadi <ali_mohammadi@ut.ac.ir>
2022-11-27 20:38:58 +0330

    Modify quiz model

--- quizes/models.py
+++ quizes/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Quiz(models.Model):
     question = models.TextField()
-    user = models.ManyToManyField(question, related_name="question")
+    user = models.ManyToManyField("question", related_name="question")
