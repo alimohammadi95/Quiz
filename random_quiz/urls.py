477704b1cd325358d3a85bc98ccb7c13072eee52
Ali Mohammadi <ali_mohammadi@ut.ac.ir>
2022-11-27 19:23:01 +0330

    Add main url to urls

--- random_quiz/urls.py
+++ random_quiz/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from quizes.views import quiz
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', quiz, name="quiz"),
 ]
