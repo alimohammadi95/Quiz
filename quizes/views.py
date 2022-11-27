d84edebbfc02957427ecdd9eee0842f41c6f59f9
Ali Mohammadi <ali_mohammadi@ut.ac.ir>
2022-11-27 20:39:37 +0330

    Modify quiz view

--- quizes/views.py
+++ quizes/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from quizes.models import Quiz
 
-# Create your views here.
+
+def quiz(request):
+    if request.method == "GET":
+        question = Quiz.objects.all("question")
+        user = None
+        context = {
+            "question": question,
+            "user": user,
+        }
+        render(request, "base.html", context=context)
+    elif request.method == "POST":
+        return HttpResponse("here is the result")
+
