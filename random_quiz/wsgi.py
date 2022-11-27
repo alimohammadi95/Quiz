57e0eca030c52f7f452c7ae4dc18a87da1992a27
Ali Mohammadi <ali_mohammadi@ut.ac.ir>
2022-11-27 18:27:14 +0330

    Nothing! just a refctoring

--- random_quiz/wsgi.py
+++ random_quiz/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for random_quiz project.
+WSGI config for random_quiz1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'random_quiz.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'random_quiz1.settings')
 
 application = get_wsgi_application()
